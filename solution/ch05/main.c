f45aa40be3d88e32f5944dfa1881178c0b75814a
tgni <nitonggui@aliyun.com>
2017-02-19 18:47:48 +0800

    move stack and que into lib

--- solution/ch05/main.c
+++ solution/ch05/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <hashquad.h>
+#include "tool.h"
 
 int main(int argc, char *argv[])
 {
-	int i, A[7] = {4371, 1323, 6173, 4199, 4344, 9679, 1989};
+	int i, A[] = {4371, 1323, 6173, 4199, 4344, 9679, 1989, 100, 200, 300, 400};
 	HashTable H;
 
-	if (!(H = InitializeTable(7))) {
+	if (!(H = InitializeTable(NELEMS(A)))) {
 		printf("Initialize hash table failed\n");
 		return -1;
 	}
 
-	for (i = 0; i < 7; ++i)
+	for (i = 0; i < NELEMS(A); ++i)
+		Insert(A[i], H);
 
+	for (i = 0; i < NELEMS(A); ++i)
+		printf("Element: %d Position: %d\n", A[i], Find(A[i], H));
 
 	return 0;
 }
